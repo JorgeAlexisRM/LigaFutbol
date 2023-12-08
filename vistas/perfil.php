2e591411cb1172ce2926235a86e672bf8b2b86f9
JorgeAlexisRM <alex.jruam@gmail.com>
2023-12-08 13:06:21 -0600

    base para plantilla

---
+++ vistas/perfil.php
@@ -0,0 +1,14 @@
+<div>
+    <h1 class="titulo">Entrenador</h1>
+</div>
+
+<div class="datosEntrenador">
+    <img src="./img/equipo.png">
+    <label>Equipo: </label><br>
+    <?php
+        $idEquipo = 'equipo657264785ac3a';
+        echo '<button onclick="window.location.href=\'index.php?vista=plantilla&idEquipo='.$idEquipo.'\'">
+            Equipo
+        </button>'
+    ?>
+</div>
