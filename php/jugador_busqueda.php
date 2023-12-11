c54b45e8aded89591642eb122af31cba785b967b
Krhono <krhono134@gmail.com>
2023-12-10 22:00:32 -0600

    Solucion de diseño de escudos y errores de jugadores sin equipo

--- php/jugador_busqueda.php
+++ php/jugador_busqueda.php
@@ -1,6 +1,6 @@
 <?php
 $item = "";
-$imf="../img/inf.png";
+$imf = "./img/inf.png";
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -16,23 +16,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
+            $row = $result->fetch_assoc();
+            $equipo = $row['equipo'];
+            if ($equipo == null) {
+                if ($result_equipo->num_rows > 0) {
+                    ($row_equipo = $result_equipo->fetch_assoc());
+                }
                 $item .= '<div class="item">
-                    <img src="'. $row['img'] . '" style="width: 120px; height: 85px;">
-                    <label>' . $row['nombre'] . '</label>
-                    <label>' . $row['apellido'] . '</label>
-                    <label>' . $row['edad'] . ' años' . '</label>
-                    <label>' . $row['equipo'] . '</label>
-                    <label>' . $row['camiseta'] . '</label>
-                    <label>' . $row['posicion'] . '</label>
-                    <a href="infoJugador.php?id='. $row['idJugador'] .'">
-  				        <img src="../img/inf.png" style="width: 35px; height: 35px; margin-top: 24px">
+			            <img src="./img/equipos/equipo.png' . '" style="width: 60px; height: 60px;">
+                        <label>' . $row['nombre'] . '</label>
+			            <label>' . $row['apellido'] . '</label>
+                        <label>' . $row['edad'] . ' años' . '</label>
+			            <label>' . $row['equipo'] . '</label>
+                        <label>' . $row['camiseta'] . '</label>
+			            <label>' . $row['posicion'] . '</label>
+			            <a href="index.php?vista=infoJugador&idJugador=' . $row['idJugador'] . '&idEquipo=' . $row['equipo'] . '">
+  				            <img src="../img/inf.png" style="width: 35px; height: 35px; margin-top: 11px">
+			            </a>
+                    </div>';
+            } else {
+                $sql_equipo = "SELECT foto FROM equipos WHERE idequipo = '$equipo'";
+                $result_equipo = $conn->query($sql_equipo);
+                if ($result_equipo->num_rows > 0) {
+                    ($row_equipo = $result_equipo->fetch_assoc());
+                }
+                $item .= '<div class="item">
+			            <img src="./img/equipos/' . $row_equipo['foto'] . '" style="width: 60px; height: 60px;">
+                        <label>' . $row['nombre'] . '</label>
+			            <label>' . $row['apellido'] . '</label>
+                        <label>' . $row['edad'] . ' años' . '</label>
+			            <label>' . $row['equipo'] . '</label>
+                        <label>' . $row['camiseta'] . '</label>
+			            <label>' . $row['posicion'] . '</label>
+			        <a href="index.php?vista=infoJugador&idJugador=' . $row['idJugador'] . '&idEquipo=' . $row['equipo'] . '">
+  			        	<img src="../img/inf.png" style="width: 35px; height: 35px; margin-top: 11px">
 			        </a>
                 </div>';
             }
             echo $item;
         } else {
-            echo '<h1 class="error">'.'No se encontro al jugador'.'</h1>';
+            echo '<h1 class="error">' . 'No se encontro al jugador' . '</h1>';
         }
 
         $conn->close();
--- php/jugador_lista.php
+++ php/jugador_lista.php
@@ -12,18 +12,39 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	while ($row = $result->fetch_assoc()) {
-		$item .= '<div class="item">
-			<img src="./img/puma.png" style="width: 120px; height: 85px;">
+		$equipo = $row['equipo'];
+		if ($equipo == null) {
+			$item .= '<div class="item">
+			<img src="./img/equipos/equipo.png' . '" style="width: 60px; height: 60px;">
+            <label>' . $row['nombre'] . '</label>
+			<label>' . $row['apellido'] . '</label>
+            <label>' . $row['edad'] . ' años' . '</label>
+			<label>' .'Sin Equipo'.'</label>
+            <label>' . $row['camiseta'] . '</label>
+			<label>' . $row['posicion'] . '</label>
+			<a href="index.php?vista=infoJugador&idJugador=' . $row['idJugador'] . '&idEquipo=' . $equipo . '">
+  				<img src="../img/inf.png" style="width: 35px; height: 35px; margin-top: 11px">
+			</a>
+            </div>';
+		} else {
+			$sql_equipo = "SELECT foto FROM equipos WHERE idequipo = '$equipo'";
+			$result_equipo = $conn->query($sql_equipo);
+			if ($result_equipo->num_rows > 0) {
+				($row_equipo = $result_equipo->fetch_assoc());
+			}
+			$item .= '<div class="item">
+			<img src="./img/equipos/' . $row_equipo['foto'] . '" style="width: 60px; height: 60px;">
             <label>' . $row['nombre'] . '</label>
 			<label>' . $row['apellido'] . '</label>
             <label>' . $row['edad'] . ' años' . '</label>
 			<label>' . $row['equipo'] . '</label>
             <label>' . $row['camiseta'] . '</label>
 			<label>' . $row['posicion'] . '</label>
-			<a href="index.php?vista=infoJugador&idJugador='. $row['idJugador'] .'&idEquipo='.$row['equipo'].'">
-  				<img src="./img/inf.png" style="width: 35px; height: 35px; margin-top: 24px">
+			<a href="index.php?vista=infoJugador&idJugador=' . $row['idJugador'] . '&idEquipo=' . $row['equipo'] . '">
+  				<img src="../img/inf.png" style="width: 35px; height: 35px; margin-top: 11px">
 			</a>
             </div>';
+		}
 	}
 	echo $item;
 }
